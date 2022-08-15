3e5090cf84bcf8892b066474a8eb7b8936ec37ff
Jake Overall <joverall22@gmail.com>
2022-08-15 10:49:36 -0600

    getting started

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <header>
+    <Navbar />
+  </header>
+  <main>
+    <router-view />
+  </main>
+  <footer>
+    <div class="bg-dark text-light text-center p-4">
+      Made with 💖 by CodeWorks
+    </div>
+  </footer>
+</template>
+
+<script>
+// this is your new controller
+import { computed } from 'vue';
+import { AppState } from './AppState';
+
+export default {
+  name: 'App',
+  setup() {
+    return {
+      //        v is the new ProxyState.on
+      appState: computed(() => AppState)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "./assets/scss/main.scss";
+
+</style>
